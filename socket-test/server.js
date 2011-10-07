b4d190869c7abd4222e549a31bb45648bb8196ed
John Q. Server <daniel.guilak+johnq@gmail.com>
2011-10-07 14:36:21 -0700

    Added connection functionality.
    The reason the client wasn't connecting is because it was trying to connect before everything in the page was parsed -- so I added a DOMContentLoaded event to make sure that doesn't happen anymore. Now it's possible just to include a javascript file in a <script> tag rather than having to embed it physically on the page.

--- socket-test/server.js
+++ socket-test/server.js
@@ -1,7 +1,14 @@
+/* Server.js for Vir-Pong, Inc as drafted by Daniel Guilak */
+
+//Require the express framework (which creates a server)
 var app = require('express').createServer(),
+//and socket.io which provides websocket support.
     sio = require('socket.io');
 
+//when someone requests / on the webserver, send them a small script
+//that will aid them in connecting to the server.
 app.get('/', function(req, res){
+  //This is all client-side now
   res.send('<script src="/socket.io/socket.io.js"></script>\
 <script>\
   var socket = io.connect("http://10.150.1.204");\
@@ -13,11 +20,16 @@ app.get('/', function(req, res){
 </script>');
 });
 
+//set the server to listen on port
 app.listen(3000);
 
+//set the sockets to listen on same port.
 var io = sio.listen(app);
 
+//when someone connects
 io.sockets.on('connection', function (socket) {
+  //emit some news
   socket.emit('news', { hello: 'world' });
+  //when I receive some news, put it in the console.
   socket.on('news', function(data) { console.log(data.hello);});
 });
