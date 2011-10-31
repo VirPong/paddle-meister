895621f1b8eebe248d0cde4a2927c90dcce20b2b
Daniel Guilak <daniel.guilak@gmail.com>
2011-10-30 22:49:14 -0700

    Added more functionality.

--- socket-test/server.js
+++ socket-test/server.js
@@ -68,10 +68,10 @@ function ballLogic(){
   // changed all these numbers to more reasonable also, these kinda stuff should also be fields but we can
   // think about that later
   
-  if((ballPos[0] <= 10) && (ballPos[0] > 5) && (ballPos[1] > paddlePos[0]) && (ballPos[1] < (paddlePos[0] + paddleSize[0]))){ //if it hits the left paddle
+  if((ballPos[0] == 10) && (ballPos[1] > paddlePos[0] - 3) && (ballPos[1] < (paddlePos[0] + paddleSize[0] + 3))){ //if it hits the left paddle
     ballV[0] = -ballV[0]; //get faster after you hit it
   }
-  if((ballPos[0] >= fieldSize[0] - 10) && (ballPos[0] <= fieldSize[0] - 5) && (ballPos[1] >= paddlePos[1]) && (ballPos[1] <= (paddlePos[1] + paddleSize[0]))){ //if it hits the right paddle
+  if((ballPos[0] == fieldSize[0] - 10) && (ballPos[1] > paddlePos[1] - 3) && (ballPos[1] < (paddlePos[1] + paddleSize[0] + 3))){ //if it hits the right paddle
     ballV[0] = -ballV[0];
   }
   
@@ -82,7 +82,7 @@ function ballLogic(){
     ballPos[1] = fieldSize[1]/2;
     ballV[0] = 1;
     ballV[1] = 2;
-    score[1]++;
+    score[1] = score[1] + 1;
     sendScore();
   }
   if(ballPos[0] >fieldSize[0] +10 ){ //changed these numbers you had old ones so ball was going super far out of frame
