e93b324284a27eface22cb4efe3e31e190dd134c
Daniel Guilak <daniel.guilak@gmail.com>
2011-11-29 21:02:22 -0800

    Fixed paddle movement issues.

--- socket-test/server.js
+++ socket-test/server.js
@@ -26,7 +26,7 @@ io.sockets.on('connection', function (aClient) {
 
   console.log("Client connecting.");
 
-  newClient = new Client(aClient, "Bobothy");
+  var newClient = new Client(aClient, "Bobothy");
   newClient.socket.emit('yeahboi');
   console.log("Created new client: " + newClient.name);
   gClients.push(newClient);
@@ -35,7 +35,7 @@ io.sockets.on('connection', function (aClient) {
   aClient.emit('roomList', {rooms: gRooms, numRooms: gNumRooms});
 
   aClient.on('joinRoom', function(data){
-    aClient.join(data.name);
+    aClient.join(data.name); //Joining socket.io 'room'
     newClient.currentRoom = gRooms[data.name]; 
     newClient.clientType = data.clientType;
     gRooms[data.name].joinRoom(newClient);
@@ -64,8 +64,8 @@ io.sockets.on('connection', function (aClient) {
   //When a client sends an updatePaddle event, record their new paddle position.
   aClient.on('paddleUpdate', function(aData) {
     //update the value of particular paddle position.
-    newClient.paddlePos = aData.pos; 
-    console.log(newClient.name + " pos:" + newClient.paddlePos);
+    newClient.setPaddlePos(aData.pos); 
+    console.log(newClient.name + " pos:" + newClient.getPaddlePos());
   });
 
 });
@@ -86,15 +86,25 @@ function Client (socket, name) {
   this.clientType;
   this.currentRoom;
 
-  this.paddlePos = 50;
+  this.paddlePos;
   this.playerNum;
+
 }
+
+Client.prototype.setPaddlePos = function(newPos) {
+  this.paddlePos = newPos;
+}
+
+Client.prototype.getPaddlePos = function() {
+  return this.paddlePos; 
+}
+
+//////////////////////////////////////////////
 
 
 //Game object!
 function Room (name) {
   this.name = name;
-  var self = this;
   /* Variable declarations */
   this.spectators = new Array();
   this.players = new Array();
@@ -102,119 +112,122 @@ function Room (name) {
   this.gameOn = false;	// Boolean whether or not the game is being played
   this.paddlePos = []; // [player1, player2] height position.
   this.ballPos = [];   // [ballX, ballY] ball positions.
-  this.ballVi = [];	// [ballVX, ballVY] ball velocities. 
+  this.ballV = [];	// [ballVX, ballVY] ball velocities. 
   this.ballR;	// The ball radius (currently not in implementation)
   this.score = [];	// [scorePlayer1, scorePlayer2] player scores.
   this.fieldSize = []; // [fieldX, fieldY] size of the game field.
   this.paddleSize;// [paddleHeight, paddleWidth]
-  
-  this.joinRoom = function(client){
-    if(client.clientType == 'player'){
-      this.players.push(client);
-      this.paddlePos[this.numPlayers] = client.paddlePos;
-      client.socket.emit('paddleID', {paddleID: this.numPlayers});
-      this.numPlayers = this.numPlayers + 1;
-      console.log("Number of players in " + this.name + ": " + this.numPlayers);
-    } 
-    else if(client.clientType == 'spectator'){
-      this.spectators.push(client);
-    }
-    
-    if(this.numPlayers == 2 && !this.gameOn){
-      this.initGame();
-      this.startGame();
-    }
-  };
-
-  /* This is the main game loop that is set to run every 50 ms. */
-  this.startGame = function(){
-      setInterval(function() {
-        self.ballLogic(); //Run ball logic simulation.
-        self.sendGameState(); //Send game state to all sockets.
-      }, 50);
-  }
-  /* 
-  Initializes the game state.
-  
-  In future revisions, magic numbers will be replaced with
-  constants and parameters.
-  */
-  this.initGame = function (){
-    console.log("Game initializing in " + this.name + "!");
-    this.ballPos = [50,50];
-    this.score = [0,0];
-    this.fieldSize = [100,100];
-    this.ballV = [1,2];
-    this.ballR = (1/20)*this.fieldSize[1];
-    //height, width
-    this.paddleSize = [(1/5)*this.fieldSize[0], (1/15)*this.fieldSize[1]];
-  };
-
-  /*
-  Helper function to send the game state to all connected clients.
-  
-  This will soon be phased out to support multiple game instances.
-  
-  Emits updateGame node event with paddle and ball position arrays.
-  */
-  this.sendGameState = function(){
-    io.sockets.in(this.name).volatile.emit('gameState', {paddle: this.paddlePos, ball: this.ballPos});
-  };
-  
-  
-  /* Sends an scoreUpdate event to all connected clients (will be phased out soon for a more
-  modular approach */
-  this.sendScore = function(){
-   //Still sending to everyone.
-   io.sockets.in(this.name).emit('scoreUpdate', { score: this.score });
-  };
-  
-  /*
-   This ball logic code originated from David Eva, slightly modified for use on the server. Needs tweaking.
-  */ 
-  
-  this.ballLogic = function(){
-  
-    //Ball bouncing logic
-    
-    if( this.ballPos[1]<0 || this.ballPos[1]>this.fieldSize[1]){
-      this.ballV[1] = -this.ballV[1]; //change gBallPos[1] direction if you go off screen in y direction ....
-    }
-    
-    // Paddle Boundary Logic
-    
-    // changed all these numbers to more reasonable also, these kinda stuff should also be fields but we can
-    // think about that later
-    
-    if((this.ballPos[0] == 10) && (this.ballPos[1] > this.paddlePos[0] - 3) && (this.ballPos[1] < (this.paddlePos[0] + this.paddleSize[0] + 3))){ //if it hits the left paddle
-      this.ballV[0] = -this.ballV[0]; //get faster after you hit it
-    }
-    if((this.ballPos[0] == this.fieldSize[0] - 10) && (this.ballPos[1] > this.paddlePos[1] - 3) && (this.ballPos[1] < (this.paddlePos[1] + this.paddleSize[0] + 3))){ //if it hits the right paddle
-      this.ballV[0] = -this.ballV[0];
-    }
-    
-    // if ball goes out of frame reset in the middle and put to default speed and increment gScore...
-    
-    if(this.ballPos[0] < -10){ //changed these numbers you had old ones so ball was going super far out of frame
-      this.ballPos[0] = this.fieldSize[0]/2;
-      this.ballPos[1] = this.fieldSize[1]/2;
-      this.ballV[0] = 1;
-      this.ballV[1] = 2;
-      this.score[1] = this.score[1] + 1;
-      this.sendScore();
-    }
-    if(this.ballPos[0] >this.fieldSize[0] +10 ){ //changed these numbers you had old ones so ball was going super far out of frame
-      this.ballPos[0] = this.fieldSize[0]/2;
-      this.ballPos[1] = this.fieldSize[1]/2;
-      this.ballV[0] = 1;
-      this.ballV[1] = 2;
-      this.score[0] = this.score[0] + 1; 
-      this.sendScore();
-    }
-    
-    this.ballPos[0]+=this.ballV[0];
-    this.ballPos[1]+=this.ballV[1];
-  };
-
 }
 
+Room.prototype.joinRoom = function(aClient){
+  if(aClient.clientType == 'player'){
+    console.log("players length: " + this.players.length);
+    //this.paddlePos[this.numPlayers] = client.paddlePos;
+    aClient.socket.emit('paddleID', {paddleID: this.players.length});
+    console.log("Player " + (this.players.length + 1) + " joined " + "(" + this.name + ")");
+    this.players.push(aClient);
+  } 
+  else if(client.clientType == 'spectator'){
+    this.spectators.push(client);
+  }
+  
+  if(this.players.length == 2 && !this.gameOn){
+    this.initGame();
+    this.startGame();
+  }
+}
+
+/* This is the main game loop that is set to run every 50 ms. */
+Room.prototype.startGame = function(){
+    var self = this;
+    setInterval(function() {
+      self.ballLogic(); //Run ball logic simulation.
+      self.sendGameState(); //Send game state to all sockets.
+    }, 50);
+}
+/* 
+Initializes the game state.
+
+In future revisions, magic numbers will be replaced with
+constants and parameters.
+*/
+Room.prototype.initGame = function (){
+  console.log("Game initializing in " + this.name + "!");
+  this.ballPos = [50,50];
+  this.score = [0,0];
+  this.fieldSize = [100,100];
+  this.ballV = [1,2];
+  this.ballR = (1/20)*this.fieldSize[1];
+  //height, width
+  this.paddleSize = [(1/5)*this.fieldSize[0], (1/15)*this.fieldSize[1]];
+
+  this.players[0].setPaddlePos(50);
+  this.players[1].setPaddlePos(50);
+}
+
+/*
+Helper function to send the game state to all connected clients.
+
+This will soon be phased out to support multiple game instances.
+
+Emits updateGame node event with paddle and ball position arrays.
+*/
+Room.prototype.sendGameState = function(){
+  var paddles = [this.players[0].getPaddlePos(), this.players[1].getPaddlePos()];
+  io.sockets.in(this.name).volatile.emit('gameState', {paddle: paddles, ball: this.ballPos});
+}
+
+
+/* Sends an scoreUpdate event to all connected clients (will be phased out soon for a more
+modular approach */
+Room.prototype.sendScore = function(){
+ //Still sending to everyone.
+ io.sockets.in(this.name).emit('scoreUpdate', { score: this.score });
+}
+
+/*
+ This ball logic code originated from David Eva, slightly modified for use on the server. Needs tweaking.
+*/ 
+
+Room.prototype.ballLogic = function(){
+
+  //Ball bouncing logic
+  
+  if( this.ballPos[1]<0 || this.ballPos[1]>this.fieldSize[1]){
+    this.ballV[1] = -this.ballV[1]; //change gBallPos[1] direction if you go off screen in y direction ....
+  }
+  
+  // Paddle Boundary Logic
+  
+  // changed all these numbers to more reasonable also, these kinda stuff should also be fields but we can
+  // think about that later
+  
+  if((this.ballPos[0] == 10) && (this.ballPos[1] > this.players[0].getPaddlePos() - 3) && (this.ballPos[1] < (this.players[0].getPaddlePos() + this.paddleSize[0] + 3))){ //if it hits the left paddle
+    this.ballV[0] = -this.ballV[0]; //get faster after you hit it
+  }
+  if((this.ballPos[0] == this.fieldSize[0] - 10) && (this.ballPos[1] > this.players[1].getPaddlePos() - 3) && (this.ballPos[1] < (this.players[1].getPaddlePos() + this.paddleSize[0] + 3))){ //if it hits the right paddle
+    this.ballV[0] = -this.ballV[0];
+  }
+  
+  // if ball goes out of frame reset in the middle and put to default speed and increment gScore...
+  
+  if(this.ballPos[0] < -10){ //changed these numbers you had old ones so ball was going super far out of frame
+    this.ballPos[0] = this.fieldSize[0]/2;
+    this.ballPos[1] = this.fieldSize[1]/2;
+    this.ballV[0] = 1;
+    this.ballV[1] = 2;
+    this.score[1] = this.score[1] + 1;
+    this.sendScore();
+  }
+  if(this.ballPos[0] >this.fieldSize[0] +10 ){ //changed these numbers you had old ones so ball was going super far out of frame
+    this.ballPos[0] = this.fieldSize[0]/2;
+    this.ballPos[1] = this.fieldSize[1]/2;
+    this.ballV[0] = 1;
+    this.ballV[1] = 2;
+    this.score[0] = this.score[0] + 1; 
+    this.sendScore();
+  }
+  
+  this.ballPos[0]+=this.ballV[0];
+  this.ballPos[1]+=this.ballV[1];
+}
