20e26de0c24d39a783f944f80bcfa6402349349e
Daniel Guilak <daniel.guilak@gmail.com>
2011-12-09 16:09:11 -0800

    Added replays.js from shelbylee's repository.

---
+++ replays.js
@@ -0,0 +1,50 @@
+/* Replays.js v.0.2 for Vir-Pong, Inc. */
+/* Shelby Lee */
+
+//want to listen on a different port than games
+var PORT = 3001;
+
+//var io = require('socket.io').listen(PORT);
+var app = require('express').createServer();
+sys = require(process.binding('natives').util ? 'util' : 'sys')
+  sio = require('socket.io');
+var io = sio.listen(app);
+var db = require('mongojs').connect('games',['replays']);
+
+var playerPos = []; //array to hold the player positions, 0 has array of p1pos and 1 p2pos
+var ballPos = []; //array holding ball positions; 0 has x-position, 1 has y-position
+
+//Connection to client
+//We send static game information to the client for them to parse in the same way
+//they parse the data we stream out of the server.js
+io.sockets.on('connection', function(aClient){
+
+  console.log("Client is connecting to replays.");
+  client = new Client(aClient, "guest");
+  //client.socket.emit('getting stuff'); //test emission
+  
+  //Requesting gameID to query on
+  aClient.on('watchGame', function(aGameID){
+    this.queryReplay(aGameID);
+    aClient.volatile.emit('replayInfo', { playerPosition : playerPos, ball Position : ballPos});
+  });
+  
+}
+
+
+
+//Helper function to query database and build player position arrays
+function queryReplay(aGameID){
+  //this query says we're looking for the games that have the same gameID as our argument
+  //we're going to leave our the mongodb generated index (_id) and the gameID
+  db.replays.find({gameID:aGameID}, {_id:0, gameID:0}).forEach(function(err, doc) {
+     //We will assume that the information in the database is in order, because arrays are inserted
+    if(doc != null) {  //don't want to run anything if it is null
+   
+      //The database store Javascript objects, we just want to hold onto them and emit them to client
+      playerPos = doc.playersPos;      //the array has entire game positions on it
+      ballPos = doc.ballPos;
+      
+    }
+  });
+}
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.listen(PORT);
 var io = sio.listen(app); 
 io.set('log level', 1); // reduce logging
 
+//Using mongojs to connect to the replays collection in the games database of mongodb
+var rDB = require('mongojs').connect('games',['replays']);
+
 var gClients = [];
 var gRooms = new Array(); 
 var gRoomNames = [];
@@ -120,6 +123,10 @@ function Room() {
   this.score = [];	// [scorePlayer1, scorePlayer2] player scores.
   this.fieldSize = []; // [fieldX, fieldY] size of the game field.
   this.paddleSize;// [paddleHeight, paddleWidth]
+
+  this.rPlayerPos = [[], []]; //[[player1], [player2]] height positions for replays
+  this.rBallPos = [[], []]; //[[ballX], [ballY]] ball positions for replays
+  this.rGameID; //the gameID that will be queried on replays
 }
 
 Room.prototype.getName = function(){
@@ -269,3 +276,37 @@ Room.prototype.ballLogic = function(){
   this.ballPos[1]+=this.ballV[1];
 }
 
+/*
+Helper functions for the rooms to communicate with the database
+Shelby Lee
+*/
+Room.prototype.cacheGameState = function(){
+  this.rPlayerPos[0].push(this.paddlePos[0]);
+  this.rPlayerPos[1].push(this.paddlePos[1]);
+  this.rBallPos[0].push(this.ballPos[0]);
+  this.rBallPos[1].push(this.ballPos[1]);
+}
+
+//Generating unique gameID
+Room.prototype.genGameID = function(){
+  //Temporarily using unix time for gameID - highly unlikely to have duplicates
+  var foo = new Date;
+  var unixtime = parseInt(foo.getTime());
+  this.rGameID = unixtime;
+    
+    /*var greatest = 0;
+db.replays.find({},{gameID:1}).forEach(function(err, doc) {
+if(doc != null){
+if(doc.gameID > greatest){
+greatest = doc.gameID;
+}
+}
+});
+this.rGameID = greatest + 1;
+*/
+}
+
+//Emitting current cached game information to database
+Room.prototype.emitReplay = function(){
+    db.replays.save({gameID: this.rGameID, playersPos : this.rPlayerPos, ballPos : this.rBallPos});
+}
