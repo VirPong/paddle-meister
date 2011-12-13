a992b62e372e9cd537eec7211b2a95606517195d
Shelby Lee <smlee@pugetsound.edu>
2011-12-13 11:03:52 -0800

    Cleaned up replay implimentation
    To make it a bit more efficient, decided that all the cached information can be saved to database while still in array form (meaning we don't have to remove, rebuild, and re-sort it). Also removed dead code and updated documentation.

--- replays.js
+++ replays.js
@@ -1,23 +1,23 @@
 /* Replays.js v.0.2 for Vir-Pong, Inc. */
 /* Shelby Lee */
 
-//want to listen on a different port than games
+//listening on a port different than games
 var PORT = 3001;
 
-//var io = require('socket.io').listen(PORT);
 var app = require('express').createServer();
 sys = require(process.binding('natives').util ? 'util' : 'sys')
   sio = require('socket.io');
 var io = sio.listen(app);
-var db = require('mongojs').connect('games',['replays']);
+var db = require('mongojs').connect('games',['replays']); //db connection
 
-//var playerPos = []; //array to hold the player positions, 0 has array of p1pos and 1 p2pos
-//var ballPos = []; //array holding ball positions; 0 has x-position, 1 has y-position
-var rDocs = []; //the javascript objects pulled from the database
+var rDocs = []; //the javascript object pulled from the database
 
-//Connection to client
-//We send static game information to the client for them to parse in the same way
-//they parse the data we stream out of the server.js
+/*
+  Client connects requesting a game based on its gameID. We send them an array of 
+  objects almost identical to the information sent during games so that they can build
+  replays easily.
+*/
+
 io.sockets.on('connection', function(aClient){
 
   console.log("Client is connecting to replays.");
@@ -33,22 +33,14 @@ io.sockets.on('connection', function(aClient){
 }
 
 
-
 //Helper function to query database and build player position arrays
 function queryReplay(aGameID){
   //this query says we're looking for the games that have the same gameID as our argument
   //we're going to leave our the mongodb generated index (_id) and the gameID
-  db.replays.find({gameID:aGameID}, {_id:0, gameID:0}).sort({index:1}).forEach(function(err, doc) {
-     //We will assume that the information in the database is in order, because arrays are inserted
-    if(doc != null) {  //don't want to run anything if it is null
-      
-      rDocs.push(doc);
-      
-      //The database store Javascript objects, we just want to hold onto them and emit them to client
-      //playerPos = doc.playersPos;      //the array has entire game positions on it
-      //ballPos = doc.ballPos;
-
-      
+  //Then we iterate over them to ensure that we don't accidentally pull something null
+  db.replays.find({gameID:aGameID}, {_id:0, gameID:0}).forEach(function(err, doc) {
+    if(doc != null) {  //ensuring not null
+      rDocs = doc;
     }
   });
 }
